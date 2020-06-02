ff8cd28b7fdcf99a26c66c824106a69259aa2205
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 03:56:22 +0700

    add create bot

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import json
+
+import telebot
+
+with open('telegram_token.json', 'r') as file:
+    data = file.read()
+config = json.loads(data)
+
+bot = telebot.TeleBot(config["token"])
