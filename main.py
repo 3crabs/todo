3d9185dcd1f8303ff36bea1f223be872bd9807e4
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-03 04:00:58 +0700

    add start and help commands

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ with open('telegram_token.json', 'r') as file:
 config = json.loads(data)
 
 bot = telebot.TeleBot(config["token"])
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет я todo bot от 3CRABS soft!\nЧтобы узнать подробности введи /help")
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Я пока ничего не умею(")
+
+
+bot.polling()
